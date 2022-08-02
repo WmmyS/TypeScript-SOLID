3fd424aa89fd057690a3dddff050a81198d17695
Wesley Santos <wesleymarcosm@gmail.com>
2022-08-02 10:12:21 -0300

    feat: Finalizado o curso

---
+++ src/5_dependency-inversion-principle/services/messeging.ts
@@ -0,0 +1,5 @@
+export class Messeging {
+  sendMessage(message: string): void {
+    console.log('Mensagem enviada:', message);
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+/*
+  Vantagens e desvantagens do S.O.L.I.D.
+  
+  Vantagens:
+
+    - Códugo modular;
+    - Código reutilizável(D.R.Y - Don't repeat yourself);
+    - Código testável, baixo acoplamento
+    - Baixo acoplamento e alta coesão
+    - Código expansível
+    - Separation of concerns - Separação de conceitos
+    - Fácil manutenção
+
+  Desvantegens:
+
+    - Complexidade
+    - Quantidade de código digitado aumenta
+    - Tempo de desenvolvimento aumenta bastante
+    - Tenha cuidados com: YAGNI (You aren't gonna need it), KISS (Keep it simple, stupid!)
+
+*/
